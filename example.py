18d2d3e040d4f3e934629fdd8156b4c0e2e0aa42
Ian Edgeley <ian@iedgeley.com>
2014-10-16 14:09:29 -0500

    Added picture

--- example.py
+++ example.py
@@ -7,7 +7,7 @@
 from Tkinter import *
 root = Tk()
 # Create the canvas widget
-drawpad = Canvas(root, background='brown')
+drawpad = Canvas(root, width=800,height=600, background='brown')
 drawpad.grid(row=0, column=1)
 # create_oval(x,y,width,height,fill color)
 oval = drawpad.create_oval(10, 50, 100, 100, fill='green')
